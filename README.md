f413a9a808075fba70b5c8e8f766d2cf7f655e83
dymafr <erwan1309@hotmail.com>
2022-03-09 19:03:26 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-ybfw6q
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-ybfw6q)
---
+++ src/components/Shop/ShopFilters.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import type { FiltersInterface, FilterUpdate } from '../../interfaces';
+defineProps<{
+  filters: FiltersInterface;
+}>();
+const emit = defineEmits<{
+  (e: 'updateFilter', filterUpdate: FilterUpdate): void;
+}>();
+</script>
+
+<template>
+  <div class="p-20 d-flex flex-column">
+    <section class="mb-20">
+      <h3 class="mb-10">Rechercher</h3>
+      <input
+        :value="filters.search"
+        @input="emit('updateFilter', { search: ($event.target as HTMLInputElement).value })"
+        type="text"
+        placeholder="Rechercher"
+      />
+    </section>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
